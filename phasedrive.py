4bf20b8ee9a42aadc3515b38a191465fe7e00b94
Tom Andersen <tom@palmerandersen.com>
2026-08-02 18:47:57 -0400

    new file, also stuff7

--- phasedrive.py
+++ phasedrive.py
@@ -162,8 +162,10 @@ def update(frame):
         
         
         # now add some noise to the amplitude to help it escape local minima
-        random_kick = 0.5 *A* random.uniform(-1,1)
-        u_osc = u_osc + random_kick
+        # the random kick should be added to the oscilator amplitude, not the wave amplitude
+        # in addition the random noise needs to be at all frequencies, not just high frequency as it is now.
+        random_kick = 2 *A* random.uniform(-1,1)
+        osc_displacement = osc_displacement + random_kick
         
 
         
